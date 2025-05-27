0d866e80ef1cf586566da701c024dcc32ea5177e
Lucas <10016755+lucqa@users.noreply.github.com>
2025-05-27 20:15:22 -0300

    Just log in case is 0

--- support/postOnBluesky.ts
+++ support/postOnBluesky.ts
@@ -3,49 +3,62 @@ import { AtpAgent } from '@atproto/api'
 
 export class PostOnBluesky {
 
-    async post(playerCount: string) {
+    async post(playerCount: any) {
 
-        // String to be posted/logged
-        const postText = 'Destiny 2 has currently ' + playerCount + ' players online on PC (Steam). #Destiny2'
+        let postText
 
-        // Starting session
-        const agent = new AtpAgent({ service: process.env.BLUESKY_SERVICE as string })
+        if ((playerCount == '0') || (playerCount == 0)) {
 
-        // Logging In
-        const login = await agent.login({
-            identifier: process.env.BLUESKY_CREDENTIAL_USER as string,
-            password: process.env.BLUESKY_CREDENTIAL_PASSWORD as string
-        })
+            // Just log if zero
+            console.log('Unable to retrieve Destiny 2 player population.')
 
-        // FF Control
-        const FFGlobalEnabled = process.env.FFGlobalEnabled as string
+        }
 
-        // FF condition
-        if (FFGlobalEnabled == "true") {
-            // Post
-            const post = await agent.post({
-                text: postText,
-                langs: ['en'],
-                facets: [
-                    {
-                        index: {
+        else {
+
+            // String to be posted/logged
+            postText = 'Destiny 2 has currently ' + playerCount + ' players online on PC (Steam). #Destiny2'
+
+            // Starting session
+            const agent = new AtpAgent({ service: process.env.BLUESKY_SERVICE as string })
+
+            // Logging In
+            const login = await agent.login({
+                identifier: process.env.BLUESKY_CREDENTIAL_USER as string,
+                password: process.env.BLUESKY_CREDENTIAL_PASSWORD as string
+            })
+
+            // FF Control
+            const FFGlobalEnabled = process.env.FFGlobalEnabled as string
+
+            // FF condition
+            if (FFGlobalEnabled == "true") {
+                // Post
+                const post = await agent.post({
+                    text: postText,
+                    langs: ['en'],
+                    facets: [
+                        {
+                            index: {
                                 byteStart: postText.length - 9,
                                 byteEnd: postText.length
-                        },
-                        features: [{
-                            $type: 'app.bsky.richtext.facet#tag',
-                            tag: 'Destiny2'
-                        }]
-                    }
-                ]
-            })
-            expect(post.uri).toBeDefined()
-            expect(post.cid).toBeDefined()
+                            },
+                            features: [{
+                                $type: 'app.bsky.richtext.facet#tag',
+                                tag: 'Destiny2'
+                            }]
+                        }
+                    ]
+                })
+                expect(post.uri).toBeDefined()
+                expect(post.cid).toBeDefined()
+            }
+            else {
+                console.log('FF is disabled.')
+            }
+            console.log(postText)
         }
-        else {
-            console.log('FF is disabled.')
-        }
-        console.log(postText)
     }
+
 
 }
